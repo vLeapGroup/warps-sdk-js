8edc90b8a325ec6c22a555652aa9159279b92a59
Micha Vie <micha@vleap.io>
2024-11-05 20:17:02 +0100

    configure registry devnet address

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -13,9 +13,9 @@ export const Config = {
 
   Registry: {
     Contract: (env: ChainEnv) => {
-      if (env === 'devnet') return ''
-      if (env === 'testnet') return ''
-      return ''
+      if (env === 'devnet') return 'erd1qqqqqqqqqqqqqpgq8h85eq9l3cp40h5s3ujqshj2x775m2wyl3tsl20ltn'
+      if (env === 'testnet') return '####'
+      return '####'
     },
   },
 }
