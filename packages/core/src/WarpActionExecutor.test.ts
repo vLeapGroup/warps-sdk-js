a10614162bf301ba58511126090fff9c280b2749
Micha Vie <micha@vleap.io>
2024-11-14 17:23:49 +0100

    impl action executor

---
+++ packages/core/src/WarpActionExecutor.test.ts
@@ -0,0 +1,83 @@
+import {
+  Address,
+  AddressValue,
+  BigUIntValue,
+  BooleanValue,
+  BytesValue,
+  U16Value,
+  U32Value,
+  U64Value,
+  U8Value,
+} from '@multiversx/sdk-core/out'
+import { WarpConfig, WarpContractAction } from './types'
+import { WarpActionExecutor } from './WarpActionExecutor'
+
+const Config: WarpConfig = {
+  env: 'devnet',
+  userAddress: 'erd1kc7v0lhqu0sclywkgeg4um8ea5nvch9psf2lf8t96j3w622qss8sav2zl8',
+}
+
+describe('WarpActionExecutor', () => {
+  it('getPositionValueFromUrl - gets the value from the url', async () => {
+    const subject = new WarpActionExecutor(Config, 'https://example.com?myvalue=2000000000000000000')
+
+    const action: WarpContractAction = {
+      type: 'contract',
+      label: 'test',
+      description: 'test',
+      address: 'erd1kc7v0lhqu0sclywkgeg4um8ea5nvch9psf2lf8t96j3w622qss8sav2zl8',
+      func: null,
+      args: [],
+      value: '0',
+      gasLimit: 1000000,
+      inputs: [
+        {
+          name: 'myvalue',
+          type: 'biguint',
+          position: 'value',
+          source: 'query',
+        },
+      ],
+    }
+
+    const actual = subject.getPositionValueFromUrl(action, 'value')
+
+    expect(actual).toBe('2000000000000000000')
+  })
+
+  it('getPositionValueFromUrl - returns null if the value is not found', async () => {
+    const subject = new WarpActionExecutor(Config, 'https://example.com')
+
+    const actual = subject.getPositionValueFromUrl({} as WarpContractAction, 'value')
+
+    expect(actual).toBeNull()
+  })
+
+  it('getTypedArgsWithInputs - returns the typed args', async () => {
+    const subject = new WarpActionExecutor(Config, 'https://example.com')
+
+    const actual = subject.getTypedArgsWithInputs({
+      args: [
+        'text:hello',
+        'uint8:1',
+        'uint16:2',
+        'uint32:3',
+        'uint64:4',
+        'biguint:5',
+        'boolean:true',
+        'address:erd1kc7v0lhqu0sclywkgeg4um8ea5nvch9psf2lf8t96j3w622qss8sav2zl8',
+      ],
+    } as WarpContractAction)
+
+    expect(actual).toEqual([
+      BytesValue.fromUTF8('hello'),
+      new U8Value(1),
+      new U16Value(2),
+      new U32Value(3),
+      new U64Value(4),
+      new BigUIntValue(5),
+      new BooleanValue(true),
+      new AddressValue(Address.fromBech32('erd1kc7v0lhqu0sclywkgeg4um8ea5nvch9psf2lf8t96j3w622qss8sav2zl8')),
+    ])
+  })
+})
--- packages/core/src/WarpActionExecutor.ts
+++ packages/core/src/WarpActionExecutor.ts
@@ -1,12 +1,73 @@
-import { Transaction } from '@multiversx/sdk-core/out'
-import { WarpAction, WarpConfig } from './types'
+import {
+  Address,
+  AddressValue,
+  BigUIntValue,
+  BooleanValue,
+  BytesValue,
+  SmartContractTransactionsFactory,
+  Transaction,
+  TransactionsFactoryConfig,
+  TypedValue,
+  U16Value,
+  U32Value,
+  U64Value,
+  U8Value,
+} from '@multiversx/sdk-core/out'
+import { getChainId } from './helpers'
+import { WarpAction, WarpActionInputPosition, WarpActionInputType, WarpConfig, WarpContractAction } from './types'
 
 export class WarpActionExecutor {
-  constructor(private config: WarpConfig) {
+  private config: WarpConfig
+  private url: URL
+
+  constructor(config: WarpConfig, url: string) {
     this.config = config
+    this.url = new URL(url)
   }
 
-  execute(action: WarpAction): Transaction {
-    throw new Error('not implemented')
+  createTransactionForExecute(action: WarpContractAction): Transaction {
+    if (!this.config.userAddress) throw new Error('WarpActionExecutor: user address not set')
+    const config = new TransactionsFactoryConfig({ chainID: getChainId(this.config.env) })
+    const factory = new SmartContractTransactionsFactory({ config })
+
+    const args = this.getTypedArgsWithInputs(action)
+    const nativeValueFromUrl = this.getPositionValueFromUrl(action, 'value')
+    const nativeTransferAmount = BigInt(nativeValueFromUrl || action.value || 0)
+
+    return factory.createTransactionForExecute({
+      sender: Address.newFromBech32(this.config.userAddress),
+      contract: Address.newFromBech32(action.address),
+      function: action.func || '',
+      gasLimit: BigInt(action.gasLimit),
+      arguments: args,
+      nativeTransferAmount,
+    })
+  }
+
+  getPositionValueFromUrl(action: WarpAction, position: WarpActionInputPosition): string | null {
+    const searchParams = new URLSearchParams(this.url.search)
+    const inputsFromQuery = action.inputs?.filter((input) => input.source === 'query')
+    const valuePositionQueryName = inputsFromQuery?.find((i) => i.position === position)?.name
+    return valuePositionQueryName ? searchParams.get(valuePositionQueryName) : null
+  }
+
+  getTypedArgsWithInputs(action: WarpContractAction): TypedValue[] {
+    return action.args.map((arg) => {
+      const [type, value] = arg.split(':')
+      return this.toTypedArg(value, type as WarpActionInputType)
+    })
+  }
+
+  private toTypedArg(arg: string, type: WarpActionInputType): TypedValue {
+    if (type === 'text') return BytesValue.fromUTF8(arg)
+    if (type === 'uint8') return new U8Value(Number(arg))
+    if (type === 'uint16') return new U16Value(Number(arg))
+    if (type === 'uint32') return new U32Value(Number(arg))
+    if (type === 'uint64') return new U64Value(BigInt(arg))
+    if (type === 'biguint') return new BigUIntValue(BigInt(arg))
+    if (type === 'boolean') return new BooleanValue(arg === 'true')
+    if (type === 'address') return new AddressValue(Address.newFromBech32(arg))
+    if (type === 'hex') return BytesValue.fromHex(arg)
+    return BytesValue.fromUTF8(arg)
   }
 }
