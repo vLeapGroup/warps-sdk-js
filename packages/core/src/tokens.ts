c71edeb10ab20c13a758fb61b89b675f3294dd0c
Micha Vie <micha@vleap.io>
2025-03-04 13:58:53 +0100

    add known token list

---
+++ packages/core/src/tokens.ts
@@ -0,0 +1,14 @@
+export type KnownToken = {
+  id: string
+  name: string
+  decimals: number
+}
+
+// Used to quickly retrieve information without needing external network requests
+// Please only add your token if 'widely' used
+export const KnownTokens: KnownToken[] = [
+  { id: 'EGLD', name: 'eGold', decimals: 18 },
+  { id: 'EGLD-000000', name: 'eGold', decimals: 18 },
+]
+
+export const findKnownTokenById = (id: string): KnownToken | null => KnownTokens.find((token) => token.id === id) || null
