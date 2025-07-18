3673576672a345f2fc495d1dcdae2a5b4f9c9245
Micha Vie <micha@vleap.ai>
2025-07-18 20:01:50 +0200

    add mvx testnet error

--- packages/adapter-multiversx/src/config.ts
+++ packages/adapter-multiversx/src/config.ts
@@ -2,6 +2,6 @@ import { WarpChainEnv } from '@vleap/warps'
 
 export const getMultiversxRegistryAddress = (env: WarpChainEnv) => {
   if (env === 'devnet') return 'erd1qqqqqqqqqqqqqpgqje2f99vr6r7sk54thg03c9suzcvwr4nfl3tsfkdl36'
-  if (env === 'testnet') return '####'
+  if (env === 'testnet') throw new Error('Multiversx testnet is not supported')
   return 'erd1qqqqqqqqqqqqqpgq3mrpj3u6q7tejv6d7eqhnyd27n9v5c5tl3ts08mffe'
 }
