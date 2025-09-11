438f86c33f064e87295370c9c040abdf003b61eb
Micha Vie <micha@vleap.ai>
2025-09-11 14:56:43 +0200

    update app secret helper

--- packages/core/src/helpers/env.ts
+++ packages/core/src/helpers/env.ts
@@ -1,7 +1,11 @@
 import { WarpConstants } from '../constants'
-import { Warp } from '../types'
+import { Warp, WarpSecret } from '../types'
 
-export const extractWarpEnvKeys = (warp: Warp) =>
+export const extractWarpSecrets = (warp: Warp): WarpSecret[] =>
   Object.values(warp.vars || {})
     .filter((val) => val.startsWith(`${WarpConstants.Vars.Env}:`))
-    .map((val) => val.replace(`${WarpConstants.Vars.Env}:`, ''))
+    .map((val) => {
+      const value = val.replace(`${WarpConstants.Vars.Env}:`, '').trim()
+      const [key, description] = value.split(WarpConstants.ArgCompositeSeparator)
+      return { key, description: description || null }
+    })
