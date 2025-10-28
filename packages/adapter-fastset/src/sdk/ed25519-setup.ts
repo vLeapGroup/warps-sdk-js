a0c17eaa31b8dac6621488519f2e5b3cea8f235e
Micha Vie <micha@vleap.ai>
2025-10-28 21:09:41 +0100

    upgrade deps

--- packages/adapter-fastset/src/sdk/ed25519-setup.ts
+++ packages/adapter-fastset/src/sdk/ed25519-setup.ts
@@ -1,5 +1,5 @@
 import * as ed from '@noble/ed25519'
-import { sha512 } from '@noble/hashes/sha512'
+import { sha512 } from '@noble/hashes/sha2.js'
 
 // Configure ed25519 library exactly like the official wallet
 ed.hashes.sha512 = sha512
